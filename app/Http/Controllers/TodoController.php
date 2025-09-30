83d11f21c1f5adb4c56fdecf6a4e223539ebf1a1
NatsuhoNakata <dwnnakata109@gmai.com>
2025-09-30 20:44:57 +0900

    ORM済

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Todo;
+
+class TodoController extends Controller
+{
+    // <ここから>
+    public function index()
+    {
+        $todo = new Todo();
+        $todos = $todo->all();
+
+        return view('todo.index'); ;
+    }
+    // <ここまで>
+}
