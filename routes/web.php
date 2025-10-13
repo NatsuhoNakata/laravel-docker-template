fe535c94930a36e55e7ff9b21ef2805bfafcd954
NatsuhoNakata <dwnnakata109@gmai.com>
2025-10-13 12:16:55 +0900

    データの取得&新規作成

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,21 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// use App\Http\Controllers\TodoController; 
+// Route::get('/todo', 'TodoController@index');
+// Route::get('/todo/create', 'TodoController@create')->name('todo.create'); // 追記
+// Route::post('/todo', 'TodoController@store')->name('todo.store');
+
+// Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
+// Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
+// Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
+
+
+
+use Illuminate\Support\Facades\Route;
 
 Route::get('/todo', 'TodoController@index');
-Route::get('/todo/create', 'TodoController@create');
+Route::get('/todo/create', 'TodoController@create')->name('todo.create');
+Route::post('/todo', 'TodoController@store')->name('todo.store');
+
+
